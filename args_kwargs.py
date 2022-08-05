804e3695d84f6ed11dbbfc61d3f0947d42f9b689
parth-283 <kp2832002@gmail.com>
2022-08-05 12:06:37 +0530

    aware to *args and **kwargs

---
+++ args_kwargs.py
@@ -0,0 +1,13 @@
+def funargs(normal,*lst,**kw):
+    print(normal)
+    for item in lst:
+        print(item)
+    for key,value in kw.items():
+        print(f"{key} : {value}")
+
+
+lst = ['Parth','hari','kevin','ravi']
+kw = {"parth": 1,"krunal": 2}
+
+normal = "This is a normal"
+funargs(normal,*lst, **kw)
