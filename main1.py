1a240943b0ac1e65ea17fe589433a45423ab1958
parth-283 <kp2832002@gmail.com>
2022-08-08 10:17:36 +0530

    If __name__==__main__ usage & necessity

---
+++ main1.py
@@ -0,0 +1,3 @@
+import main2
+
+print(main2.add(5,3))
---
+++ main2.py
@@ -0,0 +1,12 @@
+def printhar(string):
+    return f"this is String {string}"
+
+def add(num1, num2):
+    return num1 + num2 + 5
+
+print(f"{__name__} : __name__")
+
+if __name__ == '__main__':
+    print(printhar("Parth"))
+    o = add(4 , 6)
+    print(o)
