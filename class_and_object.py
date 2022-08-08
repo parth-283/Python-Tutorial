849dc3bbae52df799b37c0b8b05f945fc1708aa9
parth-283 <kp2832002@gmail.com>
2022-08-08 16:05:58 +0530

    Class And Object

---
+++ class_and_object.py
@@ -0,0 +1,15 @@
+# Classes - Template
+# Object - Instance of the Class
+
+# DRY - Do not repete yourself
+
+class Student:
+    pass
+
+
+parth = Student()
+
+parth.name = "Parth"
+parth.std = "MCA"
+
+print(parth.name)
