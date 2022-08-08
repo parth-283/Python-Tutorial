a8f170e178508f968a42f09c5941d663f3edd78b
parth-283 <kp2832002@gmail.com>
2022-08-08 10:29:04 +0530

    Join function

---
+++ join_function.py
@@ -0,0 +1,8 @@
+lst = ["Join", "Cena", "Randy", "Orton", "Shamus", "Khali", "Jinder mahal"]
+
+# for item in lst:
+#     print(item, "and", end=" ")
+
+a = " , ".join(lst)
+
+print(a,"Other WWE Superstars")
