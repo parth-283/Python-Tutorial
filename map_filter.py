fe12ff688bfe40554ed736aa49367ff48590daba
parth-283 <kp2832002@gmail.com>
2022-08-08 13:26:54 +0530

    Reduce function

--- map_filter.py
+++ map_filter.py
@@ -1,8 +1,7 @@
 '''numbers = ["2", "56", "32"]
 numbers = list(map(int, numbers))'''
 
-
-#for i in range(len(numbers)):
+# for i in range(len(numbers)):
 #   numbers[i] = int(numbers[i])
 
 # numbers[2] = numbers[2] + 1
@@ -17,7 +16,6 @@ numbers = list(map(int, numbers))'''
 square = list(map(lambda x:x*x, num))
 print(square)'''
 
-
 '''def square(a):
     return a*a
 def cube(a):
@@ -30,8 +28,7 @@ for i in range(5):
     val = list(map(lambda x:x(i),func))
     print(val)'''
 
-
-#Filter Function
+# Filter Function
 
 '''list_1 = [1,2,3,4,5,6,7,8,9,5,5,6,5]
 
@@ -44,13 +41,13 @@ print(gr_then_5)'''
 # Reduce Function
 
 from functools import reduce
-list1 = [1,2,3,5,6,8,9,4,6,4,5]
+
+list1 = [1, 2, 3, 5, 6, 8, 9, 4, 6, 4, 5]
 '''num = 0
 for i in list1:
     num = num + i
 
 print(num)'''
 
-
-num = reduce(lambda x,y:x+y, list1)
+num = reduce(lambda x, y: x + y, list1)
 print(num)
