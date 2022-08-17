3f4804bad7b3591ab76c617d74c9de14aae5b7b1
parth-283 <kp2832002@gmail.com>
2022-08-17 10:23:34 +0530

    Os Module

---
+++ Os_module.py
@@ -0,0 +1,14 @@
+import os
+
+# print(dir(os))
+# print(os.getcwd())
+# os.chdir("C://")
+# print(os.getcwd())
+# f = open("mylogs.txt")
+# print(os.listdir("C://"))
+# os.makedirs("This/that")
+# os.rename("DemoText.txt","test.txt")
+# print(os.environ.get('path'))
+# print(os.path.join("C://","test.txt"))
+# print(os.path.exists("C://"))
+# print(os.path.isfile("C://"))
