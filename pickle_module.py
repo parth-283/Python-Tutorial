85e16111696996f30f505d7aef837e7490c7bca8
parth-283 <kp2832002@gmail.com>
2022-08-18 11:22:10 +0530

    Exercise 10

--- pickle_module.py
+++ pickle_module.py
@@ -1,4 +1,5 @@
 import pickle
+import requests
 
 # Pickling a Python object
 # cars = ["Audi","BMW","Maruti","Ferary"]
@@ -13,3 +14,19 @@ import pickle
 # print(mycar)
 # print(type(mycar))
 # fileobj.close()
+
+# Exercise 10
+
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# data = requests.get(url).text
+#
+# l1 = data.split("\n")
+# l2 = [item.split(",") for item in l1 if len(item) != 0]
+#
+# with open("myiris.pkl", 'wb') as f:
+#     pickle.dump(l2, f)
+# f.close()
+
+# with open("myiris.pkl", 'rb') as f:
+#     print(pickle.load(f))
+# f.close()
